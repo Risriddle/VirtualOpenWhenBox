4f1c795d162145c723559f998799f8602954c5f6
Risriddle <risriddle62442@gmail.com>
2025-02-20 22:47:37 +0530

    fixed eslint errors

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -8,7 +8,7 @@ if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+const cached = (global as any).mongoose || { conn: null, promise: null };
 
 const dbConnect = async () => {
   if (mongoose.connection.readyState >= 1) {
